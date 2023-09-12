0d13723810f82dde70989085cd8314257ca6b7d8
Jakc3311 <sawaketsu.yu@elgana.works>
2023-09-12 18:18:03 +0900

    通用控制器

--- route/app.php
+++ route/app.php
@@ -15,3 +15,11 @@ Route::get('think', function () {
 });
 
 Route::get('hello/:name', 'index/hello');
+
+Route::get(':controller/index', 'Common/index');
+Route::get(':controller/create', 'Common/create');
+Route::get(':controller/read/:id', 'Common/read');
+Route::get(':controller/edit/:id', 'Common/edit');
+Route::put(':controller/update/:id', 'Common/update');
+Route::post(':controller/save', 'Common/save');
+Route::delete(':controller/delete/:id', 'Common/delete');
